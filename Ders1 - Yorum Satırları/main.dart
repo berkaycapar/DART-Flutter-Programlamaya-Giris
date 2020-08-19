9aac2a592d5dae88ae4c069b33ce6a9df36fb2a2
Berkay Çapar <berkaycapar@gmail.com>
2020-08-19 19:58:36 +0300

    Add files via upload

---
+++ Ders1 - Yorum Satırları/main.dart
@@ -0,0 +1,23 @@
+//Flutter Giriş 1 DART -Yorum Satırları Değişkenler, Numbers, Strings, Booleans, Diziler-Listeler, Haritalar
+
+/*
+Kodlamalara başlamadan önce gerekli kurulumları yapmış olmanız gerekmektedir.
+Kurulumun anlatıldığı yazıma şuradan ulaşabilirsiniz: LİNK.
+*/
+
+//dart programlama dili main() fonksiyonu içerisinde çalıştırılır.
+//dart programlama dilinde kodların sonuna noktalı virgül konmalıdır(;)
+main() {
+  /*
+      YORUMLAR
+  */
+  // Yorum satırları iki eğik çizginin yan yana konmasıyla oluşturulur.
+  /*
+  Çoklu yorum satırı, eğik çizgiler ve yıldız sembollerinin
+  arasında ifade edilebilir
+  */
+  print("dart programlama diline giriş"); //print() çıktı almamızı sağlar.
+  // Kısmi yorum satırı:
+  print(1 + 4 /* +7 */);
+  //bu şekilde kullanımımız dışındaki bölümlerde de yorum satırı kullanabiliriz. print'in 20 değil, 11 yazması beklenir.
+}
