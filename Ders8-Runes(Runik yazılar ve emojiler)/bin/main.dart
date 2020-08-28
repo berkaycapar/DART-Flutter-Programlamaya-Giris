5b56367d57c0fd5cf99e7f9cad1ea2096cd45b0f
Berkay Çapar <berkaycapar@gmail.com>
2020-08-28 23:00:12 +0300

    Add files via upload

---
+++ Ders8-Runes(Runik yazılar ve emojiler)/bin/main.dart
@@ -0,0 +1,10 @@
+//Flutter Giriş 1 DART -Yorum Satırları Değişkenler, Numbers, Strings, Booleans, Diziler-Listeler, Haritalar
+
+//Runes(Runik yazılar ve emojiler)
+main() {
+  //pek kullanılmıyor olsa da bilmekte fayda var.
+  Runes ifadeler = new Runes("\u2665 \u{1f605} \u{1f596}");
+  print(new String.fromCharCodes(
+      ifadeler)); //emojiler'i Char Code'larına göre gösterir
+  //new koysak da olur koymasak da.
+}
